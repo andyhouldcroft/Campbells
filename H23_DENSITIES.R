af1f2cbe5a4d5d688b2709b5e02b8ff46f5e35a7
Andrew Houldcroft <107421097+andyhouldcroft@users.noreply.github.com>
2023-11-22 21:22:44 +0000

    Add files via upload

---
+++ H23_DENSITIES.R
@@ -0,0 +1,258 @@
+library(INLA)
+library(INLAspacetime)
+library(inlabru)
+library(fmesher)
+library(sf)
+library(terra)
+
+# Set INLA mode
+bru_options_set(inla.mode = "experimental")
+
+# Import data
+data <- readRDS("data.rds")
+
+# Unwrap covariates
+covariate.1 <- unwrap(data$covariates$covariate.1)
+covariate.2 <- unwrap(data$covariates$covariate.2)
+
+# Define key parameters
+max.dist <- max(data$points$distance)
+max.cluster <- max(data$points$cluster)
+
+# Hazard-rate detection model
+log_hr <- function(distance, log.sigma, log.gamma) {
+  log1p(-exp(-(distance/exp(log.sigma))^-exp(log.gamma)))
+}
+
+# Discretized truncated log-Normal cluster size distribution
+log_pd <- function(cluster, meanlog, log.sdlog) {
+  bounds <- c(0, 1:max.cluster)
+  log(plnorm(bounds[cluster+1], meanlog = meanlog, sdlog = exp(log.sdlog)) - 
+        plnorm(bounds[cluster], meanlog = meanlog, sdlog = exp(log.sdlog))) -
+    plnorm(max(bounds), meanlog = meanlog, sdlog = exp(log.sdlog), log.p = TRUE)
+}
+
+# Define barrier SPDE Matérn models
+barrier.matern <- barrierModel.define(data$mesh, 
+                                      data$barrier,
+                                      prior.sigma = c("...", "..."),
+                                      prior.range = c("...", "..."))
+
+# Define the model components
+components <- ~ 0 + 
+  BSPDE(main = geometry, model = barrier.matern) +
+  meanlog(main = geometry, model = barrier.matern) +
+  beta.cov.1(covariate.1, model = "linear") +
+  beta.cov.2(covariate.2, model = "linear") +
+  beta.cluster(seq_len(max.cluster), model = "linear") +
+  log.sigma(1) +
+  log.gamma(1) +
+  log.sdlog(1) +
+  intercept(1) +
+  intercept.2(1)
+
+# Define the model formula
+formula <- geometry + distance + cluster ~
+  BSPDE +
+  log_hr(distance, log.sigma + beta.cluster[cluster], log.gamma) +
+  log_pd(cluster, meanlog + intercept.2, log.sdlog) +
+  beta.cov.1 +
+  beta.cov.2 +
+  intercept +
+  log(2)
+
+# Run the model
+model <- lgcp(
+  components = components,
+  data = data$points,
+  samplers = data$samplers,
+  domain = list(
+    geometry = data$mesh,
+    distance = fm_mesh_1d(seq(0, max.dist, length.out = 30)),
+    cluster = seq_len(max.cluster)),
+  formula = formula,
+  options = list(
+    bru_initial = list(beta.cov.1 = "...",
+                       beta.cov.2 = "...",
+                       beta.cluster = "...", 
+                       log.sigma = "...", 
+                       log.gamma = "...",
+                       log.sdlog = "...",
+                       intercept = "...", 
+                       intercept.2 = "..."), 
+    bru_verbose = 4, 
+    bru_method = list(rel_tol = 0.15), 
+    control.compute = list(dic = TRUE)))
+
+# View model summary
+summary(model)
+
+# Define integration points
+pred.points <- fm_int(data$mesh, data$site, format = "sf")
+
+# Predict individual abundance
+abundance <- predict(model, pred.points, ~ {
+  bounds <- c(0, 1:max.cluster)
+  cluster <- 1:max.cluster
+  cluster.expectation <- vapply(
+    seq_along(meanlog),
+    function(k){
+      prob.vector <- plnorm(
+        bounds[cluster + 1], 
+        meanlog = meanlog[k] + intercept.2, 
+        sdlog = exp(log.sdlog)) - 
+        plnorm(bounds[cluster], 
+               meanlog = meanlog[k] + intercept.2, 
+               sdlog = exp(log.sdlog))
+      prob.vector <- prob.vector/plnorm(max(bounds), 
+                                        meanlog = meanlog[k] + intercept.2, 
+                                        sdlog = exp(log.sdlog))
+      sum((1:max.cluster) * prob.vector)
+    }, 0.0)
+  sum(weight * exp(BSPDE + beta.cov.1 + beta.cov.2 + intercept) * 
+        cluster.expectation)
+}, 
+n.samples = "...")
+abundance
+
+# Predict individual abundance
+average.density <- predict(model, pred.points, ~ {
+  bounds <- c(0, 1:max.cluster)
+  cluster <- 1:max.cluster
+  cluster.expectation <- vapply(
+    seq_along(meanlog),
+    function(k){
+      prob.vector <- plnorm(
+        bounds[cluster + 1], 
+        meanlog = meanlog[k] + intercept.2, 
+        sdlog = exp(log.sdlog)) - 
+        plnorm(bounds[cluster], 
+               meanlog = meanlog[k] + intercept.2, 
+               sdlog = exp(log.sdlog))
+      prob.vector <- prob.vector/plnorm(max(bounds), 
+                                        meanlog = meanlog[k] + intercept.2, 
+                                        sdlog = exp(log.sdlog))
+      sum((1:max.cluster) * prob.vector)
+    }, 0.0)
+  sum(weight * exp(BSPDE + beta.cov.1 + beta.cov.2 + intercept) * 
+        cluster.expectation) / sum(weight)
+}, 
+n.samples = "...")
+average.density
+
+# Predict cluster abundance
+cluster.abundance <- predict(model, pred.points, ~ {
+  sum(weight * exp(BSPDE + beta.cov.1 + beta.cov.2 + intercept))
+}, 
+n.samples = "...")
+cluster.abundance
+
+# Predict mean cluster density
+average.cluster.density <- predict(model, pred.points, ~ {
+  sum(weight * exp(BSPDE + beta.cov.1 + beta.cov.2 + intercept)) / sum(weight)
+}, 
+n.samples = "...")
+average.cluster.density
+
+# Predict cluster expectation
+average.cluster.size <- predict(model, pred.points, ~ {
+  bounds <- c(0, 1:max.cluster)
+  cluster <- 1:max.cluster
+  cluster.expectation <- vapply(
+    seq_along(meanlog),
+    function(k){
+      prob.vector <- plnorm(
+        bounds[cluster + 1], 
+        meanlog = meanlog[k] + intercept.2, 
+        sdlog = exp(log.sdlog)) - 
+        plnorm(bounds[cluster], 
+               meanlog = meanlog[k] + intercept.2, 
+               sdlog = exp(log.sdlog))
+      prob.vector <- prob.vector/plnorm(max(bounds), 
+                                        meanlog = meanlog[k] + intercept.2, 
+                                        sdlog = exp(log.sdlog))
+      sum((1:max.cluster) * prob.vector)
+    }, 0.0)
+  sum(weight * cluster.expectation) / sum(weight)
+}, 
+n.samples = "...")
+average.cluster.size
+
+# Define prediction grid
+grid <- fm_pixels(data$mesh, mask = TRUE)
+
+# Predict individual density surface
+density.surface <- predict(model, grid, ~ {
+  bounds <- c(0, 1:max.cluster)
+  cluster <- 1:max.cluster
+  cluster.expectation <- vapply(
+    seq_along(meanlog),
+    function(k){
+      prob.vector <- plnorm(
+        bounds[cluster + 1], 
+        meanlog = meanlog[k] + intercept.2, 
+        sdlog = exp(log.sdlog)) - 
+        plnorm(bounds[cluster], 
+               meanlog = meanlog[k] + intercept.2, 
+               sdlog = exp(log.sdlog))
+      prob.vector <- prob.vector/plnorm(max(bounds), 
+                                        meanlog = meanlog[k] + intercept.2, 
+                                        sdlog = exp(log.sdlog))
+      sum((1:max.cluster) * prob.vector)
+    }, 0.0)
+  exp(BSPDE + beta.cov.1 + beta.cov.2 + intercept) * cluster.expectation
+}, 
+n.samples = "...")
+ggplot() + gg(density.surface, geom = "tile")
+
+# Predict cluster density surface
+cluster.density.surface <- predict(model, grid, ~ {
+  exp(BSPDE + beta.cov.1 + beta.cov.2 + intercept)
+}, 
+n.samples = "...")
+ggplot() + gg(cluster.density.surface, geom = "tile")
+
+# Predict spatial cluster distribution
+cluster.size.surface <- predict(model, grid, ~ {
+  bounds <- c(0, 1:max.cluster)
+  cluster <- 1:max.cluster
+  cluster.expectation <- vapply(
+    seq_along(meanlog),
+    function(k){
+      prob.vector <- plnorm(
+        bounds[cluster + 1], 
+        meanlog = meanlog[k] + intercept.2, 
+        sdlog = exp(log.sdlog)) - 
+        plnorm(bounds[cluster], 
+               meanlog = meanlog[k] + intercept.2, 
+               sdlog = exp(log.sdlog))
+      prob.vector <- prob.vector/plnorm(max(bounds), 
+                                        meanlog = meanlog[k] + intercept.2, 
+                                        sdlog = exp(log.sdlog))
+      sum((1:max.cluster) * prob.vector)
+    }, 0.0)
+  cluster.expectation
+}, 
+n.samples = "...")
+ggplot() + gg(cluster.size.surface, geom = "tile")
+
+# Predict barrier SPDE spatial effect
+BSPDE.spatial.effect <- predict(model, grid, ~ {
+  exp(BSPDE)
+}, 
+n.samples = "...")
+ggplot() + gg(BSPDE.spatial.effect, geom = "tile")
+
+# Predict covariate.1 spatial effect
+cov.1.spatial.effect <- predict(model, grid, ~ {
+  exp(beta.cov.1)
+}, 
+n.samples = "...")
+ggplot() + gg(cov.1.spatial.effect, geom = "tile")
+
+# Predict covariate.2 spatial effect
+cov.2.spatial.effect <- predict(model, grid, ~ {
+  exp(beta.cov.2)
+}, 
+n.samples = "...")
+ggplot() + gg(cov.2.spatial.effect, geom = "tile")
---
+++ H23_SETUP.R
@@ -0,0 +1,76 @@
+library(INLA)
+library(fmesher)
+library(sf)
+library(terra)
+library(readxl)
+
+# Set INLA mode
+bru_options_set(inla.mode = "experimental")
+
+# Define project CRS in kilometers
+crs.km <- "..."
+
+# Import detection locations, distances and cluster sizes
+points <- read_excel("~/...", sheet = "...")
+points <- st_as_sf(points, coords = c("longitude", "latitude"), crs = 4326)
+points <- st_transform(points, crs.km)
+
+# Import study site
+site <- st_read(dsn = "~/...", layer = "...")
+site <- st_transform(site, crs.km)
+
+# Import mesh hull
+hull <- st_read(dsn = "~/...", layer = "...")
+hull <- st_transform(hull, crs.km)
+
+# Import barrier polygon
+barrier <- st_read(dsn = "~/...", layer = "...")
+barrier <- st_transform(barrier, crs.km)
+
+# Import line transect repeats
+samplers <- st_read(dsn = "~/...", layer = "...")
+samplers <- st_transform(samplers, crs.km)
+
+# Import your vector data
+vector <- st_read(dsn = "~/...", layer = "...")
+vector <- st_transform(vector, crs.km)
+
+# Import your raster data
+raster <- rast("~/...")
+raster <- project(raster, crs.km)
+
+# Define triangulation mesh
+mesh <- inla.mesh.2d(boundary = hull,
+                     max.edge = c("...", "..."),
+                     crs = crs.km)
+
+# Define barrier triangles
+barrier <- unlist(fm_contains(
+  x = barrier, 
+  y = mesh, 
+  type = "centroid"))
+
+# Compute covariate grid with 25-m resolution
+grid <- rast(st_as_sf(data.frame(mesh$loc[,1:2]), 
+                      coords = c("X1", "X2"), 
+                      crs = crs.km),
+             resolution = c(0.025, 0.025))
+
+# Example 1: Proximity to vector.data in kilometers
+covariate.1 <- distance(grid, vect(vector), unit = "km")
+
+# Example 2: Mean of raster.data per 500-m buffer
+buffer <- focalMat(raster, 0.5, type = "circle")
+covariate.2 <- focal(raster, w = buffer, fun = "mean", na.rm = TRUE)
+covariate.2 <- resample(covariate.2, grid, method = "average")
+
+# Export data
+saveRDS(list(points = points, 
+             samplers = samplers, 
+             site = site,
+             hull = hull,
+             mesh = mesh,
+             barrier = barrier,
+             covariates = list(covariate.1 = wrap(covariate.1),
+                               covariate.2 = wrap(covariate.2))),
+        "data.rds")
